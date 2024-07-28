1622b4fc209fcdcab7f5664bb429b028a9c33f25
usama rahman <rahmanusama390@gmail.com>
2024-07-29 05:56:53 +0600

    12.3

--- educonnect/components/Test.jsx
+++ educonnect/components/Test.jsx
@@ -1,14 +1,22 @@
-"use client"
+"use client";
 
 import { toast } from "sonner";
 import { Button } from "./ui/button";
 
 const Test = () => {
-    const handleClick = (mode) => {
-        mode ? toast.success("Test success") : toast.error("Test error");
-    };
+  const handleClick = (mode) => {
+    mode ? toast.success("Test success") : toast.error("Test error");
+  };
 
-    return <Button variant="link" className="underline" onClick={() => handleClick(false)}>Test Toast</Button>;
+  return (
+    <Button
+      variant="link"
+      className="underline"
+      onClick={() => handleClick(false)}
+    >
+      Test Toast
+    </Button>
+  );
 };
 
 export default Test;
