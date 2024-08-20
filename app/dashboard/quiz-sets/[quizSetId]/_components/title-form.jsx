e264917395086243751a4df6ea95c49336b859bd
usama-rahman <rahmanusama390@gmail.com>
2024-08-20 16:40:14 +0600

    Formatted

--- app/dashboard/quiz-sets/[quizSetId]/_components/title-form.jsx
+++ app/dashboard/quiz-sets/[quizSetId]/_components/title-form.jsx
@@ -1,10 +1,8 @@
 "use client";
 
 import * as z from "zod";
-// import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-
 import {
   Form,
   FormControl,
