a36a0465d3457b8f4c1b60670b0da735e6565144
usama-rahman <rahmanusama390@gmail.com>
2024-08-20 16:31:36 +0600

    Formatted

--- hooks/use-lock-body.js
+++ hooks/use-lock-body.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 
-// @see https://usehooks.com/useLockBodyScroll.
+// https://usehooks.com/useLockBodyScroll.
+
 export function useLockBody() {
   React.useLayoutEffect(() => {
     const originalStyle = window.getComputedStyle(document.body).overflow;
